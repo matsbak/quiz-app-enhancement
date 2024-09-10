1a6de1d0f9ec3a84f9ddde50cd1aa6dcef36a467
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-09-10 16:27:00 +0200

    Added RetryContainer widget

---
+++ lib/retry_container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RetryContainer extends StatelessWidget {
+  const RetryContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Retry container'); // Placeholder
+  }
+}
