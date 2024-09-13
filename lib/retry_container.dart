f77554b32f21839c25d984fc6e1930fb242d843a
mathi <mathiaswestergaardjohansen@gmail.com>
2024-09-13 11:27:15 +0200

    added necessary logic for retry contrainer

--- lib/retry_container.dart
+++ lib/retry_container.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class RetryContainer extends StatelessWidget {
-  const RetryContainer({super.key});
+  const RetryContainer({
+    super.key,
+    required this.onRetry,
+  });
+
+  final void Function() onRetry;
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Retry container'); // Placeholder
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.close,
+              color: Colors.red,
+              size: 100.0,
+            ),
+            Text(
+              'You failed!',
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 230, 200, 253),
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30.0),
+            TextButton.icon(
+              onPressed: onRetry,
+              style: TextButton.styleFrom(foregroundColor: Colors.white),
+              icon: const Icon(Icons.refresh),
+              label: const Text('Retry Quiz!'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
